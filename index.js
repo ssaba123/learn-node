81e664b510061ae63bea183a5bf2cea69e704ece
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-11-26 11:08:33 -0500

    Error handling

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
 
-fs.readFile('./files/test.txt', (err,data)=>{
+fs.readFile('./files/test.txt', 'utf8', (err,data)=>{
     if(err) throw err
-    console.log(data.toString())
+    console.log(data)
+})
+
+// error handling
+process.on('uncaughtException', err=>{
+    console.error(`THERE WAS AN ERROR: ${err}`)
+    process.exit(1)
 })
